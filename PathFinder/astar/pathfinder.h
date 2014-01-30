e0873b089da26296a6389a7096dc4ffe131627d5
Sidharth Juyal <sid@whackylabs.com>
2014-01-30 08:19:40 +0530

    Updated comments

--- PathFinder/astar/pathfinder.h
+++ PathFinder/astar/pathfinder.h
@@ -20,8 +20,8 @@
 /**
  * Shortest path between start and target
  * @param startX and nStartY are the coordinates of the start position.
- * @param targetX and nTargetY are the coordinates of the target position.
- * @param map is a nMapWidth * nMapHeight large grid where traversable locations are marked with 1 and non-traversable as 0. Locations are considered to be adjacent horizontally and vertically but not diagonally.
+ * @param targetX and targetY are the coordinates of the target position.
+ * @param map is a mapWidth * mapHeight large grid where traversable locations are marked with 1 and non-traversable as 0. Locations are considered to be adjacent horizontally and vertically but not diagonally.
  * @param outBuffer is where you should output the found path and nOutBufferSize is the maximum length of a path that can be written to pOutBuffer. Entries in pOutBuffer are indices into pMap.
  * @return The length of the path between Start and Target. PATH_NONE if no path exists. PATH_BUFOVERFLOW if pOutBuffer is smaller than output.
 */
